eebb91f63644c80cdadd64b9da661cf35a66beba
CrownDrip530 <lordwhite094@gmail.com>
2026-06-09 11:59:22 +0800

    Update dllmain.cpp

--- src/CrownDrip_Core/dllmain.cpp
+++ src/CrownDrip_Core/dllmain.cpp
@@ -1,20 +1,24 @@
 #include <windows.h>
 #include "pipe.h"
 
-DWORD WINAPI CoreInitialize(LPVOID lpParam) {
-    // 1. 抹除記憶體中的 PE 頭特徵，防止 Hyperion 的記憶體掃描器（Module Sweep）抓到 DLL 結構
-    DWORD oldProtect;
-    VirtualProtect(GetModuleHandleA(NULL), 0x1000, PAGE_READWRITE, &oldProtect);
-    RtlZeroMemory(GetModuleHandleA(NULL), 0x1000); 
-
-    // 2. 開啟背景腳本監聽管道
-    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)InitializeExecutionPipe, NULL, 0, NULL);
+DWORD WINAPI CoreInitialize(LPVOID)
+{
+    InitializeExecutionPipe();
     return 0;
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
-    if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
-        CreateThread(NULL, 0, CoreInitialize, NULL, 0, NULL);
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID)
+{
+    switch (reason)
+    {
+    case DLL_PROCESS_ATTACH:
+        DisableThreadLibraryCalls(hModule);
+        CreateThread(nullptr, 0, CoreInitialize, nullptr, 0, nullptr);
+        break;
+
+    case DLL_PROCESS_DETACH:
+        break;
     }
+
     return TRUE;
 }
