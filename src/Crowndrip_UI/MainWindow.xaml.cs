29f07d4ddb1dfc5cee0a18dc5a58b6fca80d975c
CrownDrip530 <lordwhite094@gmail.com>
2026-06-09 11:55:10 +0800

    Update MainWindow.xaml.cs

--- src/Crowndrip_UI/MainWindow.xaml.cs
+++ src/Crowndrip_UI/MainWindow.xaml.cs
@@ -1,22 +1,25 @@
-using System.IO.Pipes;
-using System.Text;
+using System;
 using System.Windows;
 
-namespace Crowndrip_UI {
-    public partial class MainWindow : Window {
-        private void ExecuteButton_Click(object sender, RoutedEventArgs e) {
-            string rawScript = ScriptTextBox.Text;
-            byte[] encryptedData = EncryptPayload(rawScript); // XOR 加密防止字串被靜態攔截
-
-            using (NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", "CrownDripPipe", PipeDirection.Out)) {
-                pipeClient.Connect(1000);
-                pipeClient.Write(encryptedData, 0, encryptedData.Length);
-            }
+namespace Crowndrip_UI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
         }
-        private byte[] EncryptPayload(string input) {
-            byte[] bytes = Encoding.UTF8.GetBytes(input);
-            for (int i = 0; i < bytes.Length; i++) bytes[i] ^= 0x5A; // 簡單混淆
-            return bytes;
+
+        private void ExecuteButton_Click(object sender, RoutedEventArgs e)
+        {
+            StatusLabel.Content = "Status: Safe build - execution disabled";
+
+            MessageBox.Show(
+                "Script execution is disabled in this safe build.",
+                "CrownDrip Safe Build",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information
+            );
         }
     }
 }
